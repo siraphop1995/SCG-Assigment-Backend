b7b5df3a54e2350557be28980375c78c7f33f790
siraphop1995 <siraphop.a95@gmail.com>
2019-09-26 17:37:32 +0700

    getXYZ complete

---
+++ Helpers/helperFunction.js
@@ -0,0 +1,25 @@
+const findXYZ = () => {
+  const items = ["x", "5", "9", "15", "23", "y", "z"];
+
+  //Find location of X, Y and Z as well as add index
+  const newItems = items
+    .map((item, i) => {
+      return { name: item, id: i };
+    })
+    .filter(item => {
+      return isNaN(item.name);
+    });
+
+  //Use math formula to find the answer
+  const answers = newItems.map(item => {
+    const { id } = item;
+    let answer = Math.pow(id, 2) + 3 + id;
+    return { name: item.name, answer: answer };
+  });
+
+  return answers;
+};
+
+module.exports = {
+  findXYZ: findXYZ
+};
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
-
-
-
-
+const helper = require("./Helpers/helperFunction");
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-
+app.get("/getXYZ", (req, res) => {
+  res.send(helper.findXYZ())
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
