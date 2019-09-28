542cabe3efb7e3832689f7d62cff4fa1dfe58d0b
Siraphop1995 <southeater95@gmail.com>
2019-09-28 20:56:23 +0700

    add nextPlace api

--- api/routes/scgRouter.js
+++ api/routes/scgRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const axios = require('axios');
 const helper = require('../helpers/helperFunction');
+// const respond = require('../helpers/tempPlace');
 
 //read all
 app.get('/', (req, res) => {
@@ -27,7 +28,77 @@ app.get('/findPlace', async (req, res) => {
   const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&location=${location}&radius=${radius}&key=${key}`;
 
   const respond = await axios.get(url);
-  res.send(respond.data);
+  if (respond.data.status !== "OK") {
+    return res.status(403).send(respond.data)
+  }
+  //Map result so that server only send needed data
+  newResult = respond.data.results.map((result) => {
+    //Extract map location from html_attribute
+    const map_location = extractMap(result.photos[0].html_attributions[0], result.name)
+    return {
+      id: result.id,
+      name: result.name,
+      formatted_address: result.formatted_address,
+      rating: result.rating,
+      photoRef: result.photos[0].photo_reference,
+      icon: result.icon,
+      map_location: map_location,
+      location: result.geometry.location
+    }
+  })
+
+  //Attach status and next_page_token
+  let newPlace = {
+    next_page_token: respond.data.next_page_token,
+    results: newResult,
+    status: respond.data.status
+  }
+
+  res.send(newPlace);
+
 });
+
+app.get('/nextPlace/:token', async (req, res) => {
+  const key = 'AIzaSyCoVGJYwRwjsmawIQtF5gl1jO_sFUjpy0E';
+  const { token } = req.params
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=${token}&key=${key}`;
+
+  const respond = await axios.get(url);
+
+  if (respond.data.status !== "OK") {
+    return res.status(403).send(respond.data)
+  }
+
+  //Map result so that server only send needed data
+  newResult = respond.data.results.map((result) => {
+    //Extract map location from html_attribute
+    const map_location = extractMap(result.photos[0].html_attributions[0], result.name)
+    return {
+      id: result.id,
+      name: result.name,
+      formatted_address: result.formatted_address,
+      rating: result.rating,
+      photoRef: result.photos[0].photo_reference,
+      icon: result.icon,
+      map_location: map_location,
+      location: result.geometry.location
+    }
+  })
+
+  //Attach status and next_page_token
+  let newPlace = {
+    next_page_token: respond.data.next_page_token,
+    results: newResult,
+    status: respond.data.status
+  }
+
+  res.send(newPlace);
+
+});
+
+const extractMap = (html) => {
+  let baseHTML = html.split('"')
+  return baseHTML[1]
+};
 
 module.exports = router;
